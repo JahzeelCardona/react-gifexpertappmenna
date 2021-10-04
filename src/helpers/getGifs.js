73faba60861deeb80d8c50c687e82bebdb4d29e8
Jahzeel <hernandezcardonadiciembre@gmail.com>
2021-10-04 14:54:26 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+	const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+		category
+	)}&limit=10&api_key=yNC5KYzG5uyUTr6x3mwCAWcj2yFUejo2`;
+	const resp = await fetch(url);
+	const { data } = await resp.json();
+
+	const gifs = data.map((img) => {
+		return {
+			id: img.id,
+			title: img.title,
+			url: img.images?.downsized_medium.url,
+		};
+	});
+
+	return gifs;
+};
